9af93da71502398822b21d94b82c6b1508ab409d
Official <exedeveloping@gmail.com>
2026-01-22 19:05:50 +0300

    Create script.js

---
+++ script.js
@@ -0,0 +1,9 @@
+function order(itemName) {
+  alert(
+    "You ordered: " + itemName +
+    "\n\nNext steps:" +
+    "\n1. Tell me in school what you ordered" +
+    "\n2. Pay in school" +
+    "\n3. I deliver the brainrot"
+  );
+}
